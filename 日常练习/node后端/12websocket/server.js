33439078ad2b767770db0eca72865b3636ee4a37
zhongyufan <495241088@qq.com>
2020-09-21 18:03:25 +0800

    前后端socket

--- 日常练习/node后端/12websocket/server.js
+++ 日常练习/node后端/12websocket/server.js
@@ -1,26 +1,22 @@
-const express = require('express');
-const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+let num = 0;
 
-app.get('/', (req, res) => {
-    // res.send('<h1>Web通讯</h1>');
-    res.sendFile(__dirname + '/index.html');
-});
-
-io.on('connection', (socket) => {
-    // console.log(socket);
+// 启动服务
+io.on('connect', socket => {
+    num++;
+    console.log('连接数：', num);
+    socket.emit('connection', { msg: '服务器连接成功...', noRead: 0 });
+    let count = 0;
+    setInterval(() => {
+        socket.emit('count', { state: 1, noRead: ++count });
+    }, 3000);
     socket.on('disconnect', () => {
-        console.log('连接成功');
-    });
+        num--;
+        console.log('服务断开，当前连接数：', num);
+    })
 });
-
-http.listen(3000);
-
-// app.get('/', (req, res) => {
-//     res.send('hello,world!')
-// });
-
-// app.listen(80);
+server.listen(3000);
 
 console.log('服务器启动成功');
