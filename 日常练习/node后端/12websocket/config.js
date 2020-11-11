5ad78efe8f7a24c17258618e705fed3ac69a7e35
zhongyf <>
2020-11-11 10:40:51 +0800

    egg学习

---
+++ 日常练习/node后端/12websocket/config.js
@@ -0,0 +1,7 @@
+module.exports  = {
+    port: 30000, // 服务端口
+    redis_port: '6479',
+    redis_host: '10.0.0.121',
+    redis_db: '2',
+    redis_password: '19971212',
+}
--- 日常练习/node后端/12websocket/server.js
+++ 日常练习/node后端/12websocket/server.js
@@ -1,22 +1,24 @@
 const app = require('express')();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
+const config = require('./config'); // 导入配置文件
 
-// 查redis
+// redis配置
 const redis = require('redis');
 const session = require('express-session');
-const redisConfig = {
-    redis_port: '6479',
-    redis_host: '10.0.0.121',
-    redis_db: '2',
-    redis_password: '19971212',
-};
+// const redisConfig = {
+//     redis_port: '6479',
+//     redis_host: '10.0.0.121',
+//     redis_db: '2',
+//     redis_password: '19971212',
+// };
+// 查redis
 let RedisStore = require('connect-redis')(session);
 let redisClient = redis.createClient({
-    port: redisConfig.redis_port,
-    host: redisConfig.redis_host,
-    db: redisConfig.redis_db,
-    password: redisConfig.redis_password,
+    port: config.redis_port,
+    host: config.redis_host,
+    db: config.redis_db,
+    password: config.redis_password,
 });
 
 app.use(
@@ -29,41 +31,44 @@ app.use(
 
 let num = 0; // 连接数
 
+console.log('socket服务器启动中');
 // 启动服务
 io.on('connect', socket => {
-    let tenandId = null; // 租户ID
+    console.log('socket服务器启动成功');
+    let tenantId = null; // 租户ID
     let userId = null; // 用户ID
     let msgNum = null; // 新消息数量
     let time = null; // 计时器
     num++;
-    console.log('连接数：', num);
+    console.log('socket连接数：', num);
     socket.emit('connection', { msg: '服务器连接成功...', noRead: 0 });
     // 获取前端发送的用户数据
     socket.on('userInfo', (data) => {
-        tenandId = data.tenandId;
+        tenantId = data.tenantId;
         userId = data.userId;
         getNewMsg(socket);
     });
     time = setInterval(() => {
         getNewMsg(socket);
-        console.log(msgNum)
+        console.log(process.env.NODE_APP_INSTANCE + ':' + num + ':' + userId + ':' + msgNum);
     }, 1000);
     socket.on('disconnect', () => {
         num--;
         clearInterval(time);
-        console.log('服务断开，当前连接数：', num);
+        console.log('服务断开，当前socket连接数：', num);
     })
+
     // 查询 redis 数据
     function getNewMsg(socket) {
-        if (tenandId == null) {
-            socket.emit('customError', { state: 4, msg: 'tenandId不能为空', data: {} });
+        if (tenantId == null) {
+            socket.emit('customError', { state: 4, msg: 'tenantId不能为空', data: {} });
             return;
         }
         if (userId == null) {
             socket.emit('customError', { state: 4, msg: 'userId不能为空', data: {} });
             return;
         }
-        redisClient.get(`${tenandId}:AMS_NOTICE_MESSAGE_VALUE:${userId}`, (err, reply) => {
+        redisClient.get(`${tenantId}:AMS_NOTICE_MESSAGE_VALUE:${userId}`, (err, reply) => {
             if (err) {
                 socket.emit('customError', { state: 4, msg: 'Redis查询出错', data: err });
                 return;
@@ -74,8 +79,7 @@ io.on('connect', socket => {
         });
     };
 });
-server.listen(3000);
-
+server.listen(config.port);
 
 console.log('服务器启动成功');
 
